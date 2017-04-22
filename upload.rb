374d353ace6918a196d844924635528818eeefeb
iain <iain@iain.nl>
2017-04-22 16:33:53 -0400

    Update upload script to support gzip and cache

--- upload.rb
+++ upload.rb
@@ -1,25 +1,53 @@
 require "aws-sdk"
 
-AWS.config(
-  access_key_id: ENV["PERSONAL_AWS_ACCESS_KEY_ID"],
-  secret_access_key: ENV["PERSONAL_AWS_SECRET_ACCESS_KEY"],
-)
+require "dotenv"
+Dotenv.load
 
-objects = AWS::S3.new.buckets["www.iain.nl"].objects
+BUCKET_NAME = "www.iain.nl"
 
-Dir["out/*"].each do |file|
-  key = File.basename(file)
-  objects[key].write(
-    Pathname(file),
-    :acl => :public_read
-  )
+client = Aws::S3::Client.new
+
+content_type = ->(key) {
+  case key
+  when /\.html$/  then "text/html"
+  when /\.jpe?g$/ then "image/jpeg"
+  when /\.png$/   then "image/png"
+  when /\.ico$/   then "image/ico"
+  when "feed"     then "application/rss+xml"
+  else "text/html"
+  end
+}
+
+Dir["out/*"].each do |filename|
+  next if filename.end_with?(".gz")
+  system("gzip -9 #{filename}")
+end
+
+Dir["out/*"].each do |filename|
+  key = File.basename(filename).sub(/\.gz$/, "")
+
+  File.open(filename, "rb") do |file|
+    client.put_object(
+      acl:               "public-read",
+      key:               key,
+      body:              file,
+      bucket:            BUCKET_NAME,
+      content_type:      content_type[key],
+      content_encoding:  "gzip",
+      cache_control:     "max-age=86400",
+    )
+  end
+
   puts "Uploaded #{key}"
 end
 
-objects.each do |obj|
-  unless File.exist?("out/#{obj.key}")
-    puts "Deleting #{obj.key}"
-    obj.delete
+client.list_objects_v2(bucket: BUCKET_NAME).contents.each do |object|
+  unless File.exist?("out/#{object.key}.gz")
+    puts "Deleting #{object.key}"
+    client.delete_object(
+      bucket: BUCKET_NAME,
+      key: object.key,
+    )
   end
 end
 
